a801bb493348040ff96d1514ab2a8ecda2b71690
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-09-01 15:53:52 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="tamagotchi",
-    version="0.1.0",
+    version="0.1.1",
     description="",
     long_description="",
     packages=["tamagotchi"],
