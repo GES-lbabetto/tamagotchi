943a1baf834f37b0f2f6905d7be365ed772086f1
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-10-10 14:16:29 +0200

    typo

--- tamagotchi/1_📂_File_Manager.py
+++ tamagotchi/1_📂_File_Manager.py
@@ -151,10 +151,8 @@ with setup_tab:
         for md in ss.MDs:
             st.write(f"##### {ss.MDs[md].name}")
             for property in dir(ss.MDs[md]):
-                if not property.startswith("__"):
-                    st.write(
-                        f"* {property}: ``{ss.MDs[md].property if getattr(ss.MDs[md], property) else None}``"
-                    )
+                if not property.startswith("__") and property != name:
+                    st.write(f"* {property}: ``{getattr(ss.MDs[md], property)}``")
             # st.write(f"* output: ``{ss.MDs[md].out.name if ss.MDs[md].out else None}``")
             # st.write(f"* trajectory: ``{ss.MDs[md].xyz.name if ss.MDs[md].xyz else None}``")
             # st.write(f"* mol2: ``{ss.MDs[md].mol2.name if ss.MDs[md].mol2 else None}``")
