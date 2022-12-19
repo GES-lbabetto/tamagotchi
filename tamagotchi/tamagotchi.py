525e1f8d530945ebfedf1112182ba8303ba8da0f
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-12-19 14:43:09 +0100

    removed file size limit

--- tamagotchi/tamagotchi.py
+++ tamagotchi/tamagotchi.py
@@ -1,5 +1,8 @@
 import os
 
+
 def main():
     dirname = os.path.dirname(__file__)
-    os.system(f"streamlit run {dirname}/1_📂_File_Manager.py --server.headless true")
+    os.system(
+        f"streamlit run {dirname}/1_📂_File_Manager.py --server.headless true --server.maxUploadSize 2000"
+    )
